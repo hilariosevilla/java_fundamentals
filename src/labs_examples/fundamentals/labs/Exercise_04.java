89db5e987bba8330e9318ada39cd94668195dc55
hilariosevilla <sevhil@gmail.com>
2022-08-14 21:25:55 -0700

    undid the new

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -3,8 +3,10 @@ package labs_examples.fundamentals.labs;
 /**
  * Fundamentals Exercise 4: My Second Program
  *
- *      Write the necessary code to display, declare and print each of Java's primitive data types.
- *      Please declare a single variable of each type, give it an appropriate value, then print that variable.
+ * Write the necessary code to display, declare and print each of Java's
+ * primitive data types.
+ * Please declare a single variable of each type, give it an appropriate value,
+ * then print that variable.
  *
  */
 
@@ -14,7 +16,6 @@ public class Exercise_04 {
 
         int i = 1;
         System.out.println("int i is: " + i);
-        System.out.println("new");
         // write your code below
 
     }
